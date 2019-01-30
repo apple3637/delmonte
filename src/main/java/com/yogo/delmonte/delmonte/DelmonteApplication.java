021cf2f99e6ef5a9a02e4b439b652ffc0f82c277
stella lim <apple3637@gmail.com>
2019-01-30 22:06:49 +0900

    test

---
+++ src/main/java/com/yogo/delmonte/delmonte/DelmonteApplication.java
@@ -0,0 +1,14 @@
+package com.yogo.delmonte.delmonte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DelmonteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DelmonteApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/yogo/delmonte/delmonte/DelmonteApplicationTests.java
@@ -0,0 +1,17 @@
+package com.yogo.delmonte.delmonte;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DelmonteApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
